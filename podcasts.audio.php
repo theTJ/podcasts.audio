c4a8193603eba33e0d2740cef5dc77b9da8b9810
TJ <thetj89@googlemail.com>
2015-02-20 15:01:20 +0100

    first description for Wordpress

---
+++ podcasts.audio.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: podcasts.audio
+ * Plugin URI: http://plugin.podcasts.audio
+ * Description: Frontend for a Podcast-Directory
+ * Version: 0.0.1
+ * Author: theTJ
+ * Author URI: http://thetj.eu
+ * Text Domain: 
+ * Domain Path: 
+ * Network: false
+ * License: MIT
+ */
+ defined( 'ABSPATH' ) or die( 'This is A Wordpress-PlugIn can not run without Wordpress!' );
+ 
+ ?>
